f544761ead51c26c6bdec14f4a7d48390e1c3c65
h3nnn4n <uber.renan@gmail.com>
2025-03-22 18:48:21 -0300

    DNS setup

--- terraform/output.tf
+++ terraform/output.tf
@@ -29,3 +29,28 @@ output "staging-ip" {
 output "database-ip" {
   value = hcloud_server.postgres.ipv4_address
 }
+
+output "route53_zone_id" {
+  value       = data.aws_route53_zone.colosseum_website.zone_id
+  description = "The Route53 zone ID for colosseum.website"
+}
+
+output "main_domain" {
+  value       = aws_route53_record.main.fqdn
+  description = "The fully qualified domain name for the main website"
+}
+
+output "api_domain" {
+  value       = aws_route53_record.api.fqdn
+  description = "The fully qualified domain name for the API"
+}
+
+output "db_domain" {
+  value       = aws_route53_record.db.fqdn
+  description = "The fully qualified domain name for the database"
+}
+
+output "staging_domain" {
+  value       = aws_route53_record.staging.fqdn
+  description = "The fully qualified domain name for the staging environment"
+}
