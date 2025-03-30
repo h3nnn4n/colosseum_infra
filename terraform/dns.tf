bb84208c0cca7adabf3942999253cfe72c3c29c8
h3nnn4n <uber.renan@gmail.com>
2025-03-30 17:51:31 -0300

    Setup dedicated redis dns

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -26,6 +26,14 @@ resource "aws_route53_record" "db" {
   records = [hcloud_server.postgres.ipv4_address]
 }
 
+resource "aws_route53_record" "redis" {
+  zone_id = data.aws_route53_zone.colosseum_website.zone_id
+  name    = "redis.colosseum.website"
+  type    = "A"
+  ttl     = 300
+  records = [hcloud_server.postgres.ipv4_address]
+}
+
 resource "aws_route53_record" "celery" {
   zone_id = data.aws_route53_zone.colosseum_website.zone_id
   name    = "celery.colosseum.website"
